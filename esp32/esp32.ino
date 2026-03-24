697ab3e3dc51eee649196709146b9032346c2dd9
TrevorCD <trevorcldrwd@gmail.com>
2026-03-23 20:34:02 -0700

    added connect and disconnect callbacks, and print debug messages.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -3,6 +3,8 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+#define DEBUG 1
+
 BLECharacteristic *pCharacteristic;
 BLEServer *pServer;
 BLEService *pService;
@@ -11,17 +13,38 @@ BLEAdvertising * pAdvertising;
 char * serviceUUID = "3cd00375-4415-4fe2-aa41-42bd35f1c526";
 char * characteristicUUID = "cc84a98c-36be-4fe1-8345-be620545fd34";
 
+class ServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        #if DEBUG
+        Serial.println("Connected");
+        #endif
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+        BLEDevice::getAdvertising()->start();  // restart advertising
+        #if DEBUG
+        Serial.println("Disconnected. Advertising started");
+        #endif
+    }
+};
+
 void setup()
 {
+  #if DEBUG
+  Serial.begin(9600);
+  Serial.println("Setup started");
+  #endif
   // pin setup
 
   // BLE setup
   BLEDevice::init("ESP32MagicWand");
   pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+
   pService = pServer->createService(serviceUUID);
   
   pCharacteristic = pService->createCharacteristic(
-    "characteristicUUID",
+    characteristicUUID,
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
   );
 
@@ -30,6 +53,10 @@ void setup()
 
   pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
+
+  #if DEBUG
+  Serial.println("Advertising started");
+  #endif
 }
 
 void loop()
